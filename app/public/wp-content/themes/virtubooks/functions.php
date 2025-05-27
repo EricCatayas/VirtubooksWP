13f310850a8feca76b2884d55961eb6720621068
EricCatayas <catayasericjay@gmail.com>
2025-05-28 07:49:45 +0800

    feat: add Font Awesome CSS to app files

--- app/public/wp-content/themes/virtubooks/functions.php
+++ app/public/wp-content/themes/virtubooks/functions.php
@@ -4,13 +4,15 @@ function app_files()
   // Enqueue Bootstrap CSS
   wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css', array(), '5.3.1', 'all');
 
+  // Enqueue Font Awesome CSS
+  wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), '6.4.2', 'all');
+
   // Enqueue custom styles
   wp_enqueue_style('normalize-css', get_template_directory_uri() . '/css/normalize.css', array(), null, 'all');
   wp_enqueue_style('icomoon-css', get_template_directory_uri() . '/css/icomoon/icomoon.css', array(), null, 'all');
   wp_enqueue_style('vendor-css', get_template_directory_uri() . '/css/vendor.css', array(), null, 'all');
   wp_enqueue_style('main-styles', get_theme_file_uri('/build/style-index.css')); // from style.scss
   wp_enqueue_style('extra-styles', get_theme_file_uri('/build/index.css')); // any other styles imported
-
 
   // Enqueue Bootstrap JS
   wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.1', true);
