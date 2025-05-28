426990ecf644fa26608b89eaa3677c72ed5de3a0
EricCatayas <catayasericjay@gmail.com>
2025-05-28 09:57:06 +0800

    docs

--- app/public/wp-content/themes/virtubooks/src/index.js
+++ app/public/wp-content/themes/virtubooks/src/index.js
@@ -1,8 +1,10 @@
 import "../css/style.scss";
 
 /**
-  1. Externalize React and ReactDOM in Your Build
-    Tell your bundler (Webpack/@wordpress/scripts) not to bundle React and ReactDOM, but to use the ones already provided by WordPress. Ref: webpack.config.js
+  1. Externalize React and ReactDOM in Your Build 
+    WordPress already provides these packages as global variables
+    You should not use your own bundled versions of react and react-dom in production for WordPress in order to avoid version conflicts. 
+    Create webpack.config.js
   2. Add the `react` and `react-dom` dependencies to the `wp_enqueue_script` function 
   3. Use the classic JSX transform by setting `runtime` to `classic` in the `babel.config.js` file
  */
