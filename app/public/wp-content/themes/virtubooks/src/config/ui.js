5fbd845c84d592c521a4bf4d5e6f4f14fafecbd5
EricCatayas <catayasericjay@gmail.com>
2025-05-29 08:49:01 +0800

    feat: ui constants in config file

---
+++ app/public/wp-content/themes/virtubooks/src/config/ui.js
@@ -0,0 +1,47 @@
+export const defaultfontSizeOptions = [
+  {
+    label: "Small",
+    value: "small",
+  },
+  {
+    label: "Medium",
+    value: "medium",
+  },
+  {
+    label: "Large",
+    value: "large",
+  },
+];
+
+export const contentTypeOptions = [
+  { type: "paragraph", label: "Paragraph", icon: "fas fa-paragraph" },
+  { type: "heading", label: "Heading", icon: "fas fa-heading" },
+  { type: "image", label: "Image", icon: "fas fa-image" },
+  { type: "quote", label: "Quote", icon: "fas fa-quote-right" },
+];
+
+export const fontFamilyOptions = [
+  { label: "Arial", value: "Arial, sans-serif" },
+  { label: "Courier New", value: "'Courier New', monospace" },
+  { label: "Georgia", value: "Georgia, serif" },
+  { label: "Times New Roman", value: "'Times New Roman', serif" },
+  { label: "Verdana", value: "Verdana, sans-serif" },
+  { label: "Tahoma", value: "Tahoma, sans-serif" },
+  { label: "Trebuchet MS", value: "'Trebuchet MS', sans-serif" },
+  { label: "Impact", value: "Impact, sans-serif" },
+  { label: "Comic Sans MS", value: "'Comic Sans MS', cursive" },
+  { label: "Lucida Console", value: "'Lucida Console', monospace" },
+  { label: "Palatino Linotype", value: "'Palatino Linotype', serif" },
+  { label: "Open Sans", value: "'Open Sans', sans-serif" },
+  { label: "Roboto", value: "'Roboto', sans-serif" },
+  { label: "Lato", value: "'Lato', sans-serif" },
+  { label: "Montserrat", value: "'Montserrat', sans-serif" },
+];
+
+export const lineHeightOptions = [
+  { label: "1.0", value: "1.0" },
+  { label: "1.5", value: "1.5" },
+  { label: "2.0", value: "2.0" },
+  { label: "2.5", value: "2.5" },
+  { label: "3.0", value: "3.0" },
+];
