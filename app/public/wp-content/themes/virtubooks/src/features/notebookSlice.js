30be59af3cb4a79e3b7e9f7695cfbc79516ef86f
EricCatayas <catayasericjay@gmail.com>
2025-05-29 18:17:49 +0800

    feat: insert and delete page in notebook reducer

--- app/public/wp-content/themes/virtubooks/src/features/notebookSlice.js
+++ app/public/wp-content/themes/virtubooks/src/features/notebookSlice.js
@@ -25,6 +25,50 @@ export const notebookSlice = createSlice({
         })),
       }));
     },
+    insertBlankPage: (state, action) => {
+      const { pageId } = action.payload;
+      // Find the index of the page to insert after
+      const pageIndex = state.pages.findIndex((p) => p.id === pageId);
+      if (pageIndex !== -1) {
+        const frontPage = {
+          id: Date.now().toString(),
+          type: "sheet",
+          side: "front",
+          contents: [],
+        };
+        const backPage = {
+          id: Date.now().toString() + "-back",
+          type: "sheet",
+          side: "back",
+          contents: [],
+        };
+        const isLastPage = pageIndex === state.pages.length - 1;
+        if (isLastPage) {
+          // Insert before the last page
+          state.pages.splice(pageIndex - 1, 0, frontPage, backPage);
+        } else {
+          // Insert after the specified page
+          state.pages.splice(pageIndex + 1, 0, frontPage, backPage);
+        }
+      }
+    },
+    deletePage: (state, action) => {
+      const { pageId } = action.payload;
+      const pageIndex = state.pages.findIndex((p) => p.id === pageId);
+      const pageToDelete = state.pages[pageIndex];
+      if (
+        pageIndex !== -1 &&
+        pageToDelete.type !== "cover" &&
+        state.pages.lenght > 4
+      ) {
+        // Remove the page and its counterpart side
+        if (state.pages[pageIndex].side === "front") {
+          state.pages.splice(pageIndex, 2);
+        } else {
+          state.pages.splice(pageIndex - 1, 2);
+        }
+      }
+    },
     addContent: (state, action) => {
       const { pageId, contentIdx, newContent } = action.payload;
       const page = state.pages.find((p) => p.id === pageId);
@@ -53,7 +97,13 @@ export const notebookSlice = createSlice({
   },
 });
 
-export const { setNotebookState, addContent, updateContent, deleteContent } =
-  notebookSlice.actions;
+export const {
+  setNotebookState,
+  insertBlankPage,
+  deletePage,
+  addContent,
+  updateContent,
+  deleteContent,
+} = notebookSlice.actions;
 
 export default notebookSlice.reducer;
