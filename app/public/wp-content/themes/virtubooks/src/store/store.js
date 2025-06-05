5d556ca13670b7b5ae1f2bb25c42230e56ae4f60
EricCatayas <catayasericjay@gmail.com>
2025-06-05 16:03:06 +0800

    update: integrate imageSelector reducer to store

--- app/public/wp-content/themes/virtubooks/src/store/store.js
+++ app/public/wp-content/themes/virtubooks/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notebookReducer from "../features/notebookSlice";
+import imageSelectorReducer from "../features/imageSelectorSlice";
 
 export default configureStore({
   reducer: {
     notebook: notebookReducer,
+    imageSelector: imageSelectorReducer,
   },
 });
