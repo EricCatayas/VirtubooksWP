8acd56074346594fd6ccfd1efc96f65adfce0f03
EricCatayas <catayasericjay@gmail.com>
2025-06-05 16:02:15 +0800

    refactor: image uploads component

--- app/public/wp-content/themes/virtubooks/src/components/image-uploads/image-uploads.component.js
+++ app/public/wp-content/themes/virtubooks/src/components/image-uploads/image-uploads.component.js
@@ -6,7 +6,7 @@ import {
   selectImage,
 } from "../../features/imageSelectorSlice";
 
-export default function ImageUploadsComponent({ onSelectImage, styles = {} }) {
+export default function ImageUploadsComponent() {
   const dispatch = useDispatch();
   const [images, setImages] = useState([]);
   const API_URL = process.env.API_BASE_URL;
@@ -46,43 +46,41 @@ export default function ImageUploadsComponent({ onSelectImage, styles = {} }) {
   };
 
   return (
-    <div style={styles}>
-      <div className="container image-uploads-container">
-        <h2 className="mb-2">Image Uploads</h2>
-        <div className="image-upload-grid">
-          {images.map((image) => (
-            <div
-              key={image.id}
-              className="image-upload-item"
-              style={{ cursor: "pointer" }}
-              onClick={() => handleSelectImage(image)}
-            >
-              <img src={image.imageURL} alt={image.filename} />
-              <p>{image.filename}</p>
-            </div>
-          ))}
-        </div>
-        <div className="row">
-          <div className="col d-flex justify-content-end">
-            <button className="btn btn-small btn-outline-accent me-2">
-              <i className="fa fa-plus" aria-hidden="true"></i>{" "}
-              <a href="/image-uploads" target="_black">
-                Upload images
-              </a>{" "}
-            </button>
-            <button
-              className="btn btn-small btn-outline-accent me-2"
-              onClick={handleRemove}
-            >
-              Remove Image
-            </button>
-            <button
-              className="btn btn-small btn-outline-accent"
-              onClick={handleClose}
-            >
-              Close
-            </button>
+    <div className="container image-uploads-container">
+      <h2 className="mb-2">Image Uploads</h2>
+      <div className="image-upload-grid">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="image-upload-item"
+            style={{ cursor: "pointer" }}
+            onClick={() => handleSelectImage(image)}
+          >
+            <img src={image.imageURL} alt={image.filename} />
+            <p>{image.filename}</p>
           </div>
+        ))}
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-end">
+          <button className="btn btn-small btn-outline-accent me-2">
+            <i className="fa fa-plus" aria-hidden="true"></i>{" "}
+            <a href="/image-uploads" target="_black">
+              Upload images
+            </a>{" "}
+          </button>
+          <button
+            className="btn btn-small btn-outline-accent me-2"
+            onClick={handleRemove}
+          >
+            Remove Image
+          </button>
+          <button
+            className="btn btn-small btn-outline-accent"
+            onClick={handleClose}
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
