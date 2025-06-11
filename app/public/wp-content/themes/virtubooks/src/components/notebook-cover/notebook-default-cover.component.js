c17412a2709cbaf93399e33c4534be9ec521501b
EricCatayas <catayasericjay@gmail.com>
2025-06-11 09:20:54 +0800

    feat: notebook default cover component

---
+++ app/public/wp-content/themes/virtubooks/src/components/notebook-cover/notebook-default-cover.component.js
@@ -0,0 +1,51 @@
+import PageContent from "../page-content/page-content.component";
+import React from "react";
+import "../notebook/notebook.styles.css";
+import "../page/notebook-page.styles.css";
+
+export default function NotebookDefaultCover({
+  page,
+  className,
+  aspectRatio,
+  styles = {},
+}) {
+  const isReadOnly = true;
+
+  return (
+    <article className={`book ${className}`} data-aspect-ratio={aspectRatio}>
+      <section className={`page active`}>
+        <div
+          className="front" // e.g. front, or back
+          style={{
+            ...(page.backgroundImageURL
+              ? {
+                  backgroundImage: `url(${page.backgroundImageURL})`,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }
+              : {}),
+            ...styles,
+            ...(page.styles || {}),
+          }}
+        >
+          <section className="page-header">
+            {/* {page.header && ( <PageHeader /> )}  */}
+          </section>
+          <section className="page-body">
+            {page.contents.map((content, idx) => (
+              <PageContent
+                key={idx}
+                content={content}
+                isReadOnly={isReadOnly}
+              />
+            ))}
+          </section>
+          <section className="page-footer">
+            {/* {page.footer && ( <PageFooter /> )} */}
+          </section>
+        </div>
+      </section>
+    </article>
+  );
+}
