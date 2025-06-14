227790ec2b421cf4664cceb4d65817db8d3bc7bd
EricCatayas <catayasericjay@gmail.com>
2025-06-14 13:07:28 +0800

    refactor: front-page

--- app/public/wp-content/themes/virtubooks/front-page.php
+++ app/public/wp-content/themes/virtubooks/front-page.php
@@ -24,49 +24,18 @@ get_header();
 
 <section id="featured-books" class="py-5 my-5" data-aos="fade-up"></section>
 
-<!-- For smaller screen widths, replace grid with this -->
-<section id="best-selling" class="leaf-pattern-overlay">
-	<div class="corner-pattern-overlay"></div>
-	<div class="container">
-		<div class="row justify-content-center">
-
-			<div class="col-md-8">
-
-				<div class="row">
-
-					<div class="col-md-6">
-						<figure class="products-thumb">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/single-image.jpg" alt="book" class="single-image">
-						</figure>
-					</div>
-
-					<div class="col-md-6">
-						<div class="product-entry">
-							<h2 class="section-title divider">Best Selling Book</h2>
-
-							<div class="products-content">
-								<div class="author-name">By Timbur Hood</div>
-								<h3 class="item-title">Birds gonna be happy</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet,
-									libero ipsum enim pharetra hac.</p>
-								<div class="item-price">$ 45.00</div>
-								<div class="btn-wrap">
-									<a href="#" class="btn-accent-arrow">shop it now <i
-											class="icon icon-ns-arrow-right"></i></a>
-								</div>
-							</div>
-
-						</div>
-					</div>
-
-				</div>
-				<!-- / row -->
-
-			</div>
-
-		</div>
+<section id="quotation" class="align-center pb-5 mb-5">
+	<div class="inner-content">
+		<h2 class="section-title divider">Quote for You</h2>
+		<blockquote data-aos="fade-up">
+			<q>“The more that you read, the more things you will know. The more that you learn, the more places
+				you’ll go.”</q>
+			<div class="author-name">Dr. Seuss</div>
+		</blockquote>
 	</div>
 </section>
+
+<!-- todo: Notebooks from other users -->
 
 <section id="popular-books" class="bookshelf py-5 my-5">
 	<div class="container">
@@ -488,14 +457,46 @@ get_header();
 	</div>
 </section>
 
-<section id="quotation" class="align-center pb-5 mb-5">
-	<div class="inner-content">
-		<h2 class="section-title divider">Quote for You</h2>
-		<blockquote data-aos="fade-up">
-			<q>“The more that you read, the more things you will know. The more that you learn, the more places
-				you’ll go.”</q>
-			<div class="author-name">Dr. Seuss</div>
-		</blockquote>
+<section id="best-selling" class="leaf-pattern-overlay">
+	<div class="corner-pattern-overlay"></div>
+	<div class="container">
+		<div class="row justify-content-center">
+
+			<div class="col-md-8">
+
+				<div class="row">
+
+					<div class="col-md-6">
+						<figure class="products-thumb">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/single-image.jpg" alt="book" class="single-image">
+						</figure>
+					</div>
+
+					<div class="col-md-6">
+						<div class="product-entry">
+							<h2 class="section-title divider">Best Selling Book</h2>
+
+							<div class="products-content">
+								<div class="author-name">By Timbur Hood</div>
+								<h3 class="item-title">Birds gonna be happy</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet,
+									libero ipsum enim pharetra hac.</p>
+								<div class="item-price">$ 45.00</div>
+								<div class="btn-wrap">
+									<a href="#" class="btn-accent-arrow">shop it now <i
+											class="icon icon-ns-arrow-right"></i></a>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+				<!-- / row -->
+
+			</div>
+
+		</div>
 	</div>
 </section>
 
