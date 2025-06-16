54d5c238fee820a31bbe03840657170ec9465cf7
EricCatayas <catayasericjay@gmail.com>
2025-06-16 10:44:46 +0800

    feat: all-notebooks page

--- app/public/wp-content/themes/virtubooks/page-notebook.php
+++
@@ -1,7 +0,0 @@
-<?php
-get_header();
-?>
-<div>todo: Display User Notebooks</div>
-<?php
-get_footer();
-?>
---
+++ app/public/wp-content/themes/virtubooks/page-notebooks.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+
+<section id="all-books" class="bookshelf pb-5 mb-5">
+
+  <div class="section-header align-center">
+    <div class="title">
+      <span>Grab your opportunity</span>
+    </div>
+    <h2 class="section-title">All notebooks</h2>
+  </div>
+
+  <div id="all-notebooks"></div>
+</section>
+<?php
+get_footer();
+?>
