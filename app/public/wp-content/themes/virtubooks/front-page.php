0f6a0e95e9bbc8e260126a2470efe0eaeb59c477
EricCatayas <catayasericjay@gmail.com>
2025-06-17 15:17:13 +0800

    update: home page remove client holder section

--- app/public/wp-content/themes/virtubooks/front-page.php
+++ app/public/wp-content/themes/virtubooks/front-page.php
@@ -3,25 +3,6 @@ get_header();
 ?>
 <section id="billboard"></section>
 
-
-<section id="client-holder" data-aos="fade-up">
-	<div class="container">
-		<div class="row">
-			<div class="inner-content">
-				<div class="logo-wrap">
-					<div class="grid">
-						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/client-image1.png" alt="client"></a>
-						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/client-image2.png" alt="client"></a>
-						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/client-image3.png" alt="client"></a>
-						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/client-image4.png" alt="client"></a>
-						<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/client-image5.png" alt="client"></a>
-					</div>
-				</div><!--image-holder-->
-			</div>
-		</div>
-	</div>
-</section>
-
 <section id="featured-books" class="py-5 my-5" data-aos="fade-up"></section>
 
 <section id="popular-books" class="bookshelf py-5 my-5"></section>
@@ -32,7 +13,6 @@ get_header();
 		<?php
 		$quotes = new WP_Query(array(
 			'post_type' => 'quote',
-			'posts_per_page' => 1,
 			'orderby' => 'rand'
 		));
 		if ($quotes->have_posts()) {
