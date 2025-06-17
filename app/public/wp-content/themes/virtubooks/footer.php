40b5e3dc5f83d62e8ad760c8858b0629dab4e2cc
EricCatayas <catayasericjay@gmail.com>
2025-06-17 15:50:13 +0800

    update: footer

--- app/public/wp-content/themes/virtubooks/footer.php
+++ app/public/wp-content/themes/virtubooks/footer.php
@@ -1,158 +1,120 @@
 <footer id="footer">
-		<div class="container">
-			<div class="row">
+	<div class="container">
+		<div class="row">
 
-				<div class="col-md-4">
+			<div class="col-md-6">
 
-					<div class="footer-item">
-						<div class="company-brand">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/main-logo.png" alt="logo" class="footer-logo">
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis sed ptibus liberolectus
-								nonet psryroin. Amet sed lorem posuere sit iaculis amet, ac urna. Adipiscing fames
-								semper erat ac in suspendisse iaculis.</p>
+				<div class="footer-item">
+					<div class="company-brand">
+						<div class="main-logo mb-2">
+							<a href="<?php echo site_url(); ?>">
+								<h1>Virtubooks</h1>
+							</a>
 						</div>
+						<p>
+							Virtubooks is a platform for creating, sharing, and discovering digital notebooks. Whether you're a student, professional, or creative, Virtubooks helps you organize your thoughts and ideas like you're working with a real notebook.
+						</p>
 					</div>
-
-				</div>
-
-				<div class="col-md-2">
-
-					<div class="footer-menu">
-						<h5>About Us</h5>
-						<ul class="menu-list">
-							<li class="menu-item">
-								<a href="#">vision</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">articles </a>
-							</li>
-							<li class="menu-item">
-								<a href="#">careers</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">service terms</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">donate</a>
-							</li>
-						</ul>
-					</div>
-
-				</div>
-				<div class="col-md-2">
-
-					<div class="footer-menu">
-						<h5>Discover</h5>
-						<ul class="menu-list">
-							<li class="menu-item">
-								<a href="#">Home</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Books</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Authors</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Subjects</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Advanced Search</a>
-							</li>
-						</ul>
-					</div>
-
-				</div>
-				<div class="col-md-2">
-
-					<div class="footer-menu">
-						<h5>My account</h5>
-						<ul class="menu-list">
-							<li class="menu-item">
-								<a href="#">Sign In</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">View Cart</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">My Wishtlist</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Track My Order</a>
-							</li>
-						</ul>
-					</div>
-
-				</div>
-				<div class="col-md-2">
-
-					<div class="footer-menu">
-						<h5>Help</h5>
-						<ul class="menu-list">
-							<li class="menu-item">
-								<a href="#">Help center</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Report a problem</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Suggesting edits</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">Contact us</a>
-							</li>
-						</ul>
-					</div>
-
 				</div>
 
 			</div>
-			<!-- / row -->
+
+			<div class="col-md-2">
+
+				<div class="footer-menu">
+					<h5>Discover</h5>
+					<ul class="menu-list">
+						<li class="menu-item">
+							<a href="/">Home</a>
+						</li>
+						<li class="menu-item">
+							<a href="#featured-books">Featured</a>
+						</li>
+						<li class="menu-item">
+							<a href="#popular-books">Popular</a>
+						</li>
+						<li class="menu-item">
+							<a href="<?php echo esc_url(site_url('/blog')); ?>">Blog</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+			<div class="col-md-2">
+
+				<div class="footer-menu">
+					<h5>My account</h5>
+					<ul class="menu-list">
+						<?php if (is_user_logged_in()) { ?>
+							<li class="menu-item">
+								<a href="<?php echo esc_url(site_url('/profile')); ?>">Profile</a>
+							</li>
+							<li class="menu-item">
+								<a href="<?php echo esc_url(site_url('/notebooks/user/' . get_current_user_id())); ?>">My Notebooks</a>
+							</li>
+							<li class="menu-item">
+								<a href="<?php echo wp_logout_url('/login'); ?>">Log Out</a>
+							</li>
+
+						<?php } else { ?>
+							<li class="menu-item">
+								<a href="<?php echo wp_login_url(); ?>">Sign In</a>
+							</li>
+							<li class="menu-item">
+								<a href="<?php echo wp_registration_url(); ?>">Sign Up</a>
+							</li>
+						<?php } ?>
+
+					</ul>
+				</div>
+
+			</div>
+			<div class="col-md-2">
+
+				<div class="footer-menu">
+					<h5>Help</h5>
+					<ul class="menu-list">
+						<li class="menu-item">
+							<a href="#">Help center</a>
+						</li>
+						<li class="menu-item">
+							<a href="#">Report a problem</a>
+						</li>
+						<li class="menu-item">
+							<a href="#">Contact us</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
 
 		</div>
-	</footer>
+		<!-- / row -->
 
-	<div id="footer-bottom">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
+	</div>
+</footer>
 
-					<div class="copyright">
-						<div class="row">
+<div id="footer-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
 
-							<div class="col-md-6">
-								<p>© 2022 All rights reserved. Free HTML Template by <a
-										href="https://www.templatesjungle.com/" target="_blank">TemplatesJungle</a></p>
-							</div>
+				<div class="copyright">
+					<div class="row">
 
-							<div class="col-md-6">
-								<div class="social-links align-right">
-									<ul>
-										<li>
-											<a href="#"><i class="icon icon-facebook"></i></a>
-										</li>
-										<li>
-											<a href="#"><i class="icon icon-twitter"></i></a>
-										</li>
-										<li>
-											<a href="#"><i class="icon icon-youtube-play"></i></a>
-										</li>
-										<li>
-											<a href="#"><i class="icon icon-behance-square"></i></a>
-										</li>
-									</ul>
-								</div>
-							</div>
-
+						<div class="col-md-6">
+							<p>© 2022 All rights reserved.</p>
 						</div>
-					</div><!--grid-->
 
-				</div><!--footer-bottom-content-->
-			</div>
+					</div>
+				</div><!--grid-->
+
+			</div><!--footer-bottom-content-->
 		</div>
 	</div>
+</div>
 
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 </body>
 
